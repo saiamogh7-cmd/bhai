f08b358651e986dd7659dfc0b6910ea247f96c19
Team KAY <teamkay@example.com>
2026-07-05 15:51:15 +0530

    Fix: Set CORS allow_credentials to False to allow wildcard origins to resolve correctly

--- backend/app/main.py
+++ backend/app/main.py
@@ -39,7 +39,7 @@ if not origins:
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
